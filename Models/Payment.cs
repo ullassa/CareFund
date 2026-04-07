ff69f9f8bf0fe11ab2c402a7b96d9b2424d9236c
ShreyaUdupi05 <shreyakunder2@gmail.com>
2026-04-07 10:37:34 +0530

    Models adding

---
+++ Models/Payment.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+// jhgfhg
+ 
+public class Payment
+{
+    [Key]
+    public int PaymentId { get; set; }
+ 
+    [ForeignKey("Donation")]
+    public int DonationId { get; set; }
+ 
+    public decimal Amount { get; set; }
+ 
+    public PaymentStatus PaymentStatus { get; set; }
+ 
+    public string PaymentMethod { get; set; }
+ 
+    public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
+ 
+    // Navigation
+    public Donation Donation { get; set; }
+}
